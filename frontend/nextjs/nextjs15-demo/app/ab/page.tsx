60cabbbadb3a0262854a20a8beab37c53c67d851
Deer404 <919187569@qq.com>
2024-10-22 14:37:34 +0800

    feat: 添加hono+rpc+nextjs15的demo

---
+++ frontend/nextjs/nextjs15-demo/app/ab/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { hc } from "hono/client";
+import { useQuery } from "@tanstack/react-query";
+import { AppType } from "../api/[[...route]]/route";
+import { Suspense } from "react";
+const client = hc<AppType>("");
+const Layout = ({ children }: React.PropsWithChildren) => (
+  <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      {children}
+    </main>
+  </div>
+);
+export default function Home() {
+  const query = useQuery({
+    queryKey: ["query"],
+    queryFn: async () => {
+      const res = await client.api.hello.$get({
+        query: {
+          name: "hono",
+        },
+      });
+      return await res.json();
+    },
+  });
+  if (query.isLoading) {
+    return <Layout>Loading</Layout>;
+  }
+  return (
+    <Layout>
+      <span>{query.data?.message}</span>
+    </Layout>
+  );
+}
---
+++ frontend/nextjs/nextjs15-demo/app/api/[[...route]]/route.ts
@@ -0,0 +1,29 @@
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { z } from "zod";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+const route = app.get(
+  "/hello",
+  zValidator(
+    "query",
+    z.object({
+      name: z.string(),
+    })
+  ),
+  (c) => {
+    const { name } = c.req.valid("query");
+    return c.json({
+      message: `Hello! ${name}`,
+    });
+  }
+);
+
+export const GET = handle(app);
+export const POST = handle(app);
+
+export type AppType = typeof route;
