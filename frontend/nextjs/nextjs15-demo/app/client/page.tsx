9eae4d9faee286a99b7082f81a091407d261b97a
Deer404 <919187569@qq.com>
2024-10-22 15:07:31 +0800

    pref: 修改ab文件夹名字为client

--- frontend/nextjs/nextjs15-demo/app/client/page.tsx
+++ frontend/nextjs/nextjs15-demo/app/client/page.tsx
@@ -3,7 +3,6 @@
 import { hc } from "hono/client";
 import { useQuery } from "@tanstack/react-query";
 import { AppType } from "../api/[[...route]]/route";
-import { Suspense } from "react";
 const client = hc<AppType>("");
 const Layout = ({ children }: React.PropsWithChildren) => (
   <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
